b040c8a8d30990c0dbf505056411041f8ccfdabf
Lawrence <lawrencekkyalo@gmail.com>
2019-04-10 10:50:39 +0300

    Update index.java

--- index.java
+++ index.java
@@ -30,3 +30,4 @@ regex = /[F-P]/; // matches all uppercase letters F to P (inclusive)
 regex = /[0-9]/; // matches all digits
 regex = /[5-9]/; // matches any digit from 5 to 9 (inclusive)
 regex = /[a-zA-Z]/; // matches all lowercase and uppercase letters
+regex = /[^a-zA-Z]/; // matches non-letters
