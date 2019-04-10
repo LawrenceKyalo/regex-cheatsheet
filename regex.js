da55246580355657a552a9616b605c048c1ab0e0
Lawrence <lawrencekkyalo@gmail.com>
2019-04-10 11:49:09 +0300

    Update regex.js

--- regex.js
+++ regex.js
@@ -3,4 +3,4 @@ let regex;
 /* matching a specific string */
 regex = /hello/;
 regex = /hello/i;
-regex = /hello/g;
+regex = /hello/g;  //looks for multiple occurrences of string between the forward slashes...
