ccc3b1ca5e703b2de9a61b0b374d43bbd6f6a4eb
Lawrence <lawrencekkyalo@gmail.com>
2019-04-10 11:49:34 +0300

    Update regex.js

--- regex.js
+++ regex.js
@@ -2,5 +2,5 @@ let regex;
 
 /* matching a specific string */
 regex = /hello/;
-regex = /hello/i;
+regex = /hello/i;  //looks for the string between the forward slashes (case-insensitive)... matches "hello", "HelLo", "123HelLO"
 regex = /hello/g;  //looks for multiple occurrences of string between the forward slashes...
