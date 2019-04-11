1ec54cb3c70df46df79664cdffeb667c160addf8
Lawrence <lawrencekkyalo@gmail.com>
2019-04-11 11:09:50 +0300

    Update regex.js

--- regex.js
+++ regex.js
@@ -8,3 +8,5 @@ regex = /hello/g;  //looks for multiple occurrences of string between the forwar
 /* wildcards */
 regex = /h.llo/; // the "." matches any one character other than a new line character... matches "hello", "hallo" but not "h\nllo"
 regex = /h.*llo/; // the "*" matches any character(s) zero or more times... matches "hello", "heeeeeello", "hllo", "hwarwareallo"
+
+/* shorthand character classes */
