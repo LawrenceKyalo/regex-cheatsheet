f5f08ff678f9a886726851530fc64bf28027a8de
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2025-07-24 14:25:42 +0300

    re.search

--- regex.py
+++ regex.py
@@ -1,5 +1,9 @@
 import re
 
 #Searching string start and end
+
+#Check if the string starts with "The" and ends with "Spain":
+# This will return a match object if the string matches the pattern, otherwise it returns None.
 txt = "The rain in Spain"
 x = re.search("^The.*Spain$", txt)
+
